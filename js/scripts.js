cd2f0df68512376ad3f6fb94256fe20d32e583e4
rweathers89 <146603179+rweathers89@users.noreply.github.com>
2023-10-12 22:07:06 -0700

    added pokemon array
    each object has at least 3 properties

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,12 @@
+// create the array for Pokemon and list at least 3 in the array
+let pokemonList = [
+    {name:'Charizard' , height: 5.7 , type: ['Fire', 'Flying']},
+    {name: 'Nidoqueen', height: 4.3, type: ['Poison, Ground']},
+    {name: 'Gyarados', height: 21.4, type: ['Water', 'Flying']},
+    {name: 'Tentacruel', height: 5.3, type: ['Water', 'Poison']},
+    {name: 'Moltres', height: 6.7, type: ['Fire', 'Flying']}
+] // end of pokemonList Array
+
 
 
 
