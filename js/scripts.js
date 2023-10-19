ff2d69ba1fa0c9af8325bc6cd4b5be4c4847f1dd
rweathers89 <146603179+rweathers89@users.noreply.github.com>
2023-10-18 22:41:38 -0700

    added for loop for pokemon height
    conditions added for average, short, and tall heights

--- js/scripts.js
+++ js/scripts.js
@@ -7,6 +7,23 @@ let pokemonList = [
     {name: 'Moltres', height: 6.7, type: ['Fire', 'Flying']}
 ] // end of pokemonList Array
 
-
+/*this for loop will run through each pokemon listed in the array
+until it completes the loop. The height has a condition to list which
+pokemon are short, average height, or tall using the if and else if 
+conditions */
+for (let i = 0; i < pokemonList.length; i++) {
+    if (pokemonList[i].height < 15.5 && pokemonList[i].height > 5.5) {
+      document.write(pokemonList[i].name + ' ' + '(height:' + pokemonList[i].height + ')' + "<br>");
+      console.log(pokemonList[i].name + pokemonList[i].height + 'is average height.');
+    } //end average height condition
+    else if (pokemonList[i].height < 5.5) {
+      document.write(pokemonList[i].name + " " + '(height:' + pokemonList[i].height + ')' + "<br>");
+      console.log(pokemonList[i].name + pokemonList[i].height + 'is short.');
+    } //end short height condition
+    else {
+      document.write(pokemonList[i].name + " " + '(height:' + pokemonList[i].height + ')' + ' Wow! That\'s tall! <br>');
+    } //end tall height condition
+  
+  } //End for loop of pokemon height
 
 
