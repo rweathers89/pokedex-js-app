a0bf51c7bb14886628945fc2814172db89bdf086
rweathers89 <146603179+rweathers89@users.noreply.github.com>
2023-11-16 10:42:48 -0800

    added modal to js code
    modal added to give more info to each pokemon once clicked

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,57 @@
 let pokemonRepository = (function() {
   let pokemonList = [];
-  let apiURL = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
-//END pokeapi Loadlist
+  let apiURL = 'https://pokeapi.co/api/v2/pokemon/?limit=150'; //END pokeapi Loadlist  
+  let modalContainer = document.querySelector('#modal-container');
+
+  function showModal(pokemon) {
+    modalContainer.innerHTML = '';
+
+    let modal = document.createElement('div');
+    modal.classList.add('modal');
+
+    let closeButtonElement = document.createElement('button');
+    closeButtonElement.classList.add('modal-close');
+    closeButtonElement.innerText = 'Close';
+    closeButtonElement.addEventListener('click', hideModal);
+
+    let titleElement = document.createElement('h1');
+    titleElement.innerText = pokemon.name;
+
+    let contentElement = document.createElement('p');
+    contentElement.innerText = 'Pokemon height:' + ' ' + pokemon.height;
+
+    let pokemonImage = document.createElement("img");
+    pokemonImage.src = pokemon.imageUrl;
+
+
+    modal.appendChild(closeButtonElement);
+    modal.appendChild(titleElement);
+    modal.appendChild(contentElement);
+    modalContainer.appendChild(modal);
+    modal.appendChild(pokemonImage);
+
+    modalContainer.classList.add('is-visible');
+    console.log('showModal ran');
+  }
+
+  function hideModal() {
+    modalContainer.classList.remove('is-visible');
+  }
+
+  window.addEventListener('keydown', (e) => {
+    if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
+      hideModal();
+    }
+  });
+
+  modalContainer.addEventListener('click', (e) => {
+    // Since this is also triggered when clicking INSIDE the modal
+    // We only want to close if the user clicks directly on the overlay
+    let target = e.target;
+    if (target === modalContainer) {
+      hideModal();
+    }
+  });
 
 
   function getAll() {
@@ -20,35 +70,36 @@ let pokemonRepository = (function() {
     button.classList.add('button_class');
     button.addEventListener('click', function() {
       showDetails(pokemon)
+      console.log("addListItem is running");
     });
     listPokemon.appendChild(button);
     pokemonList.appendChild(listPokemon);
   }
 
-function loadList() {
-  return fetch(apiURL)
-    .then (function (response) {
-      return response.json();
-    })
-    .then (function (json) {
-      json.results.forEach(function (item) {
-        let pokemon = {
-          name: item.name,
-          detailsUrl: item.url
-        };
-        add (pokemon)
-      }); //end forEach loop
-    })
-    .catch (function (e) { //end .then json function
-      console.error(e);
-    });
-}//END loadList function
+  function loadList() {
+    return fetch(apiURL)
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(json) {
+        json.results.forEach(function(item) {
+          let pokemon = {
+            name: item.name,
+            detailsUrl: item.url
+          };
+          add(pokemon)
+        }); //end forEach loop
+      })
+      .catch(function(e) { //end .then json function
+        console.error(e);
+      });
+  }//END loadList function
 
-function loadDetails(item) {
-  let url = item.detailsUrl;
+  function loadDetails(item) {
+    let url = item.detailsUrl;
     return fetch(url)
       .then(function(response) {
-       return response.json();
+        return response.json();
       })
       .then(function(details) {
         //now add details to the item
@@ -56,31 +107,35 @@ function loadDetails(item) {
         item.height = details.height;
         item.types = details.types;
       })
-      .catch (function (e) {
-       console.error(e);
+      .catch(function(e) {
+        console.error(e);
       });
-} //end loadDetails function
+  } //end loadDetails function
 
-function add(pokemon) {
-  pokemonList.push(pokemon);
-}
+  function add(pokemon) {
+    pokemonList.push(pokemon);
+  }
 
-function showDetails(pokemon) {
-  loadDetails(pokemon).then(function() {
-    console.log(pokemon)
-  }); //END showDetails function
-}
+  function showDetails(pokemon) {
+    loadDetails(pokemon).then(function() {
+      let modalContainer = document.getElementById("modal-container");
+      modalContainer.innerHTML = pokemon.name;
+      showModal(pokemon); //be sure to add showMOdal to load pokemon items in moda
+      console.log(pokemon)
+    }); //END showDetails function
+  }
 
-return {
-  getAll: function () {
-    return pokemonList;
-  },
-  add: add,
-  addListItem: addListItem,
-  loadList: loadList,
-  loadDetails: loadDetails
-};
-})(); 
+  return {
+    getAll: function() {
+      return pokemonList;
+    },
+    add: add,
+    addListItem: addListItem,
+    loadList: loadList,
+    loadDetails: loadDetails,
+    showModal: showModal
+  };
+})();
 
 pokemonRepository.loadList().then(function() {
   //now data is loaded
@@ -88,93 +143,3 @@ pokemonRepository.loadList().then(function() {
     pokemonRepository.addListItem(pokemon);
   });
 });
-
-
-
-
-
-
-
-
-//original work
-// create the array for Pokemon and list at least 3 in the array
-/*let pokemonList = [
-  { name: 'Charizard', height: 5.7, type: ['Fire', 'Flying'] },
-  { name: 'Nidoqueen', height: 4.3, type: ['Poison, Ground'] },
-  { name: 'Gyarados', height: 21.4, type: ['Water', 'Flying'] },
-  { name: 'Tentacruel', height: 5.3, type: ['Water', 'Poison'] },
-  { name: 'Moltres', height: 6.7, type: ['Fire', 'Flying'] }
-] // end of pokemonList Array
-*/
-
-
-/*this for loop will run through each pokemon listed in the array
-until it completes the loop. The height has a condition to list which
-pokemon are short, average height, or tall using the if and else if 
-conditions 
-
-for (let i = 0; i < pokemonList.length; i++) {
-    if (pokemonList[i].height < 15.5 && pokemonList[i].height > 5.5) {
-      document.write(pokemonList[i].name + ' ' + '(height:' + pokemonList[i].height + ')' + "<br>");
-      console.log(pokemonList[i].name + pokemonList[i].height + 'is average height.');
-    } //end average height condition
-    else if (pokemonList[i].height < 5.5) {
-      document.write(pokemonList[i].name + " " + '(height:' + pokemonList[i].height + ')' + "<br>");
-      console.log(pokemonList[i].name + pokemonList[i].height + 'is short.');
-    } //end short height condition
-    else {
-      document.write(pokemonList[i].name + " " + '(height:' + pokemonList[i].height + ')' + ' Wow! That\'s tall! <br>');
-    } //end tall height condition
-
-  } //End for loop of pokemon height
-*/
-
-//create IIFE for pokemonList, fist create pokemonRepositry variable
-/*let pokemonRepository = (function() {
-  let pokemonList = [
-    { name: 'Charizard', height: 5.7, type: ['Fire', 'Flying'] },
-    { name: 'Nidoqueen', height: 4.3, type: ['Poison, Ground'] },
-    { name: 'Gyarados', height: 21.4, type: ['Water', 'Flying'] },
-    { name: 'Tentacruel', height: 5.3, type: ['Water', 'Poison'] },
-    { name: 'Moltres', height: 6.7, type: ['Fire', 'Flying'] }
-  ];*/
-
-//console.log(pokemonRepository.getAll())
-//pokemonRepository.add({ name: 'Xatu', height: 4.11, type: ['Psychic', 'Flying'] });
-//console.log(pokemonRepository.getAll());
-//End pokemonList IIFE
-
-/*
-pokemonList.forEach(function(pokemon) {
-  document.write("<p>" + pokemon.name + ' ' + '(height:' + pokemon.height + ')');
-  console.log("<p>" + pokemon.name + ' ' + '(height:' + pokemon.height + ')');
-}); //End forEach look of pokemon height
-*/
-
-/*
-pokemonRepository.getAll().forEach(function(pokemon) {
-  document.write("<p>" + pokemon.name + ' ' + '(height:' + pokemon.height + ')')
-
-}); //End forEach loop of pokemon height with IIFE pokemonRepository
-*/
-
-//console.log(pokemonRepository.getAll())
-//console.log(pokemonRepository.getAll());
-//End pokemonList IIFE
-
-//pokemonRepository.getAll().forEach(function(pokemon) {
-  //pokemonRepository.addListItem(pokemon);
-//}); //End forEach look of pokemon height with IIFE pokemonRepository
-
-
-/*let listItem = document.createElement('li');
-let button = document.createElement('button');
-console.log(button.innerText);
-button.innerText = 
-*/
-
-//console.log(pokemonRepository.getAll())
-//console.log(pokemonRepository.getAll());
-//End pokemonList IIFE
-
-
