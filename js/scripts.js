96ac478a3bda9060061247f959f9ed86cf24c6e5
rweathers89 <146603179+rweathers89@users.noreply.github.com>
2023-11-30 19:00:09 -0800

    updated showModal function
    use Bootstrap's modal method

--- js/scripts.js
+++ js/scripts.js
@@ -3,43 +3,19 @@ let pokemonRepository = (function() {
   let apiURL = 'https://pokeapi.co/api/v2/pokemon/?limit=150'; //END pokeapi Loadlist  
   let modalContainer = document.querySelector('#modal-container');
 
-  function showModal(pokemon) {
+ 
+    function showModal(pokemon) {
+      let modalTitle = document.querySelector('#modal-container .modal-title');
+      let modalBody = document.querySelector('#modal-container .modal-body');
+  
+      modalTitle.innerText = 'Pokemon name: ' + pokemon.name;
+      modalBody.innerHTML = 'Pokemon height: ' + pokemon.height + '<br><img src="' + pokemon.imageUrl + '">';
+  
+      $('#modal-container').modal('show'); // Use Bootstrap's modal method to show the modal
 
-    let modalBody = $(".modal-body");
-    let modalTitle = $(".modal-title");
-    let modalHeader = $(".modal-header");
-
-    modalTitle.empty();
-    modalBody.empty();
-
-    //create element for name in modal
-    let titleElement = $("<h1>" + pokemon.name + "</h1>");
-
-    //create img in modal content
-    let pokemonImage = document.createElement("img");
-    pokemonImage.src = pokemon.imageUrl;
-
-    //create eleent for height in modal
-    let heightElement = $("<p>" + "height: " + pokemon.height + "</p>");
-
-    //create element for weight in modal
-    let weightElement = $("<p>" + "weight: " + pokemon.weight + "</p>");
-
-    //create element for types in modal
-    let typesElement = $("<p>" + "types: " + pokemon.types + "</p>");
-
-    modalTitle.append(titleElement);
-    modalBody.append(pokemonImage);
-    modalBody.append(heightElement);
-    modalBody.append(weightElement);
-    modalBody.append(typesElement);
-
-    $("#exampleModal").click(function () {
-      $(button).toggle("modal");
-    });
-    
-console.log('showModal ran');
-  } //END showModal function
+     
+      console.log('showModal ran');
+    } //END showModal function
 
   
   function hideModal() {
